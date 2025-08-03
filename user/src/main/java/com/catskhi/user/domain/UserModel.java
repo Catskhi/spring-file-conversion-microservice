7d49328676a7fa834c2a7862732af0564e66e7cd
catskhi <catskhi@proton.me>
2025-08-03 02:41:32 -0300

    feat: Added user entity with user properties

---
+++ user/src/main/java/com/catskhi/user/domain/UserModel.java
@@ -0,0 +1,31 @@
+package com.catskhi.user.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private String id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String name;
+
+    @NotBlank
+    @Email
+    @Column(unique = true, nullable = false)
+    private String email;
+}
