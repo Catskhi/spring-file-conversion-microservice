0fc666a3ab610febb2b850ece11f822554f1edb3
catskhi <catskhi@proton.me>
2025-08-03 03:50:18 -0300

    feat: added EmailModel to create email entity

--- mail/src/main/java/com/catskhi/mail/domain/EmailModel.java
+++ mail/src/main/java/com/catskhi/mail/domain/EmailModel.java
@@ -1,4 +1,35 @@
 package com.catskhi.mail.domain;
 
+import com.catskhi.mail.enums.EmailStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "emails")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class EmailModel {
+
+    private final long SerialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false)
+    private UUID id;
+    private UUID userId;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    private String body;
+    private LocalDateTime sentAt;
+    private EmailStatus status;
+
 }
