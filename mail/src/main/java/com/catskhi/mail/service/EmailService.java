1e8baf4e5737f2ba4029febd720456db32120242
catskhi <catskhi@proton.me>
2025-08-03 03:51:05 -0300

    feat: added EmailService to save emails on database and send them, using transactional to prevent errors

--- mail/src/main/java/com/catskhi/mail/service/EmailService.java
+++ mail/src/main/java/com/catskhi/mail/service/EmailService.java
@@ -1,4 +1,38 @@
 package com.catskhi.mail.service;
 
+import com.catskhi.mail.domain.EmailModel;
+import com.catskhi.mail.enums.EmailStatus;
+import com.catskhi.mail.repositories.EmailRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private EmailRepository emailRepository;
+
+    @Transactional
+    public void sendAndSaveEmail(EmailModel emailModel) {
+        emailModel.setStatus(EmailStatus.PENDING);
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getBody());
+            mailSender.send(message);
+            emailModel.setStatus(EmailStatus.SENT);
+        } catch (Exception e) {
+            emailModel.setStatus(EmailStatus.FAILED);
+            System.out.println("Failed to send email: " + e.getMessage());
+        } finally {
+            emailRepository.save(emailModel);
+        }
+        emailRepository.save(emailModel);
+    }
 }
