3bdba3199a44a3003b6b5da85a79857a3f33cfc8
catskhi <catskhi@proton.me>
2025-08-03 06:15:27 -0300

    refactor: added user producer and fixed types of methods

--- user/src/main/java/com/catskhi/user/service/UserService.java
+++ user/src/main/java/com/catskhi/user/service/UserService.java
@@ -1,22 +1,29 @@
 package com.catskhi.user.service;
 
 import com.catskhi.user.domain.UserModel;
+import com.catskhi.user.producer.UserProducer;
 import com.catskhi.user.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserService {
     @Autowired
     private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
+    @Autowired
+    private final UserProducer userProducer;
+
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
-    public UserModel getUserById(String id) {
+    public UserModel getUserById(UUID id) {
         return userRepository.findById(id).orElse(null);
     }
 
@@ -24,11 +31,13 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    @Transactional
     public UserModel saveUser(UserModel user) {
+        userProducer.sendUserCreateEvent(user);
         return userRepository.save(user);
     }
 
-    public UserModel updateUser(String id, UserModel userDetails) {
+    public UserModel updateUser(UUID id, UserModel userDetails) {
         UserModel user = userRepository.findById(id).orElse(null);
         if (user != null) {
             user.setName(userDetails.getName());
@@ -38,7 +47,7 @@ public class UserService {
         return null;
     }
 
-    public UserModel deleteUser(String id) {
+    public UserModel deleteUser(UUID id) {
         UserModel user = userRepository.findById(id).orElse(null);
         if (user != null) {
             userRepository.delete(user);
