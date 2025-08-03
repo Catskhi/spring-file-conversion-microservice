d2719a863f85b6dd943d438be557ae8738f6f020
catskhi <catskhi@proton.me>
2025-08-03 06:13:30 -0300

    feat: added userproducer class

---
+++ user/src/main/java/com/catskhi/user/producer/UserProducer.java
@@ -0,0 +1,29 @@
+package com.catskhi.user.producer;
+
+import com.catskhi.user.configuration.RabbitMqConfig;
+import com.catskhi.user.domain.UserModel;
+import com.catskhi.user.dto.EmailDto;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserProducer {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private RabbitMqConfig rabbitMqConfig;
+
+    public void sendUserCreateEvent(UserModel userModel) {
+        EmailDto dto = new EmailDto();
+        dto.setEmailTo(userModel.getEmail());
+        dto.setEmailSubject("Welcome to the application!");
+        dto.setBody("Hello " + userModel.getName() + ",\n\nThank you for registering with us!");
+
+        rabbitTemplate.setMessageConverter(rabbitMqConfig.jackson2JsonMessageConverter());
+        rabbitTemplate.convertAndSend(rabbitMqConfig.queue().getName(), dto);
+        System.out.println(" [x] Sent: '" + dto + "'");
+    }
+}
