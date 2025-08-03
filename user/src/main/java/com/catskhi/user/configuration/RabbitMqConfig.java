1717d4c1499b670d071182c4c85d1d14a41bbf99
catskhi <catskhi@proton.me>
2025-08-03 06:10:23 -0300

    refactor: added configuration of RabbitMqConfig

--- user/src/main/java/com/catskhi/user/configuration/RabbitMqConfig.java
+++ user/src/main/java/com/catskhi/user/configuration/RabbitMqConfig.java
@@ -1,4 +1,20 @@
 package com.catskhi.user.configuration;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitMqConfig {
+    private final String queueName = "email-queue";
+
+    public Queue queue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 }
