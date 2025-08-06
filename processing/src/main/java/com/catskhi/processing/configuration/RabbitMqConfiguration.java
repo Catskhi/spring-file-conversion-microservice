0d03c26baaeffd8ff5d25983baf5628f38f37827
catskhi <catskhi@proton.me>
2025-08-06 18:06:22 -0300

    feat: added RabbitMqConfiguration to processing service

---
+++ processing/src/main/java/com/catskhi/processing/configuration/RabbitMqConfiguration.java
@@ -0,0 +1,22 @@
+package com.catskhi.processing.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfiguration {
+    private final String queueName = "video-processing-queue";
+
+    public Queue queue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+}
