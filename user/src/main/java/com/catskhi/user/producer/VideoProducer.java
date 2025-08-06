c60ed2457b50b3caf48b1ef12eca1a02120345a6
catskhi <catskhi@proton.me>
2025-08-06 20:37:08 -0300

    refactor: changed parameters from string to VideoDto

--- user/src/main/java/com/catskhi/user/producer/VideoProducer.java
+++ user/src/main/java/com/catskhi/user/producer/VideoProducer.java
@@ -1,6 +1,7 @@
 package com.catskhi.user.producer;
 
 import com.catskhi.user.configuration.RabbitMqConfig;
+import com.catskhi.user.dto.VideoDto;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -13,9 +14,9 @@ public class VideoProducer {
     @Autowired
     private RabbitMqConfig rabbitMqConfig;
 
-    public void sendVideoProcessingEvent(String videoId) {
+    public void sendVideoProcessingEvent(VideoDto videoDto) {
         rabbitTemplate.setMessageConverter(rabbitMqConfig.jackson2JsonMessageConverter());
-        rabbitTemplate.convertAndSend(rabbitMqConfig.videoProcessingQueue().getName(), videoId);
-        System.out.println(" [x] Sent video processing request for video ID: '" + videoId + "'");
+        rabbitTemplate.convertAndSend(rabbitMqConfig.videoProcessingQueue().getName(), videoDto);
+        System.out.println(" [x] Sent video processing request for video ID: '" + videoDto.getVideoId() + "'");
     }
 }
