13c0a3c8ab56bb1388bc187316e2aeb731be7f69
catskhi <catskhi@proton.me>
2025-08-06 18:05:03 -0300

    chore: added ProcessingConsumer class to consume video-processing-queue on processing service

---
+++ processing/src/main/java/com/catskhi/processing/consumer/ProcessingConsumer.java
@@ -0,0 +1,24 @@
+package com.catskhi.processing.consumer;
+
+import com.catskhi.processing.service.ProcessingService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProcessingConsumer {
+
+    @Autowired
+    final ProcessingService processingService;
+
+    public ProcessingConsumer(ProcessingService processingService) {
+        this.processingService = processingService;
+    }
+
+    @RabbitListener(queues = "video-processing-queue")
+    public void consumeVideoProcessingQueue(String videoId) {
+        System.out.println("Received message from video-processing-queue: " + videoId);
+        processingService.processVideo(videoId);
+        System.out.println("Processed video with ID: " + videoId);
+    }
+}
