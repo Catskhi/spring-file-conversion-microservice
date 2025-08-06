dec6d246714286f7beb3f99bb209f61b1937ddf0
catskhi <catskhi@proton.me>
2025-08-06 18:07:37 -0300

    feat: add VideoUploadException for handling video upload errors

---
+++ user/src/main/java/com/catskhi/user/exception/VideoUploadException.java
@@ -0,0 +1,7 @@
+package com.catskhi.user.exception;
+
+public class VideoUploadException extends RuntimeException {
+    public VideoUploadException(String message) {
+        super(message);
+    }
+}
