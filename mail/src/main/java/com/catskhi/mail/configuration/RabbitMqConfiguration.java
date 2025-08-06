1fd4cc0703b62b9d690c642af732cf1c40162c71
catskhi <catskhi@proton.me>
2025-08-06 20:33:46 -0300

    refactor: added new queue to handle more rabbitmq queues

--- mail/src/main/java/com/catskhi/mail/configuration/RabbitMqConfiguration.java
+++ mail/src/main/java/com/catskhi/mail/configuration/RabbitMqConfiguration.java
@@ -1,17 +1,23 @@
 package com.catskhi.mail.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfiguration {
-    private final String queueName = "email-queue";
+    private final String userEmailQueueName = "user-email-queue";
+    private final String videoEmailQueueName = "video-email-queue";
 
-    public Queue queue() {
-        return new Queue(queueName, true);
+    @Bean
+    public Queue userEmailQueue() {
+        return new Queue(userEmailQueueName, true);
+    }
+
+    @Bean Queue videoEmailQueue() {
+        return new Queue(videoEmailQueueName, true);
     }
 
     @Bean
