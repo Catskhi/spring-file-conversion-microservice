f1687dd609775fcd522e2e8fba1dd28eb6b806ef
catskhi <catskhi@proton.me>
2025-08-06 20:34:12 -0300

    refactor: added email exchange to send emails

--- processing/src/main/java/com/catskhi/processing/configuration/RabbitMqConfiguration.java
+++ processing/src/main/java/com/catskhi/processing/configuration/RabbitMqConfiguration.java
@@ -1,6 +1,7 @@
 package com.catskhi.processing.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -8,15 +9,21 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfiguration {
+    private final String exchangeName = "email";
     private final String queueName = "video-processing-queue";
 
+    @Bean
+    public DirectExchange emailExchange() {
+        return new DirectExchange(exchangeName, true, false);
+    }
+
+    @Bean
     public Queue queue() {
         return new Queue(queueName, true);
     }
 
     @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        return new Jackson2JsonMessageConverter(objectMapper);
+        return new Jackson2JsonMessageConverter();
     }
 }
