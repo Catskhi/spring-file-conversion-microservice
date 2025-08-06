5958f961eb0b4830ea3e1825509a50a0c8dc2af2
catskhi <catskhi@proton.me>
2025-08-06 20:36:36 -0300

    feat: added VideoEmailDto

---
+++ processing/src/main/java/com/catskhi/processing/dto/VideoEmailDto.java
@@ -0,0 +1,42 @@
+package com.catskhi.processing.dto;
+
+import java.util.UUID;
+
+public class VideoEmailDto {
+    private UUID userId;
+    private String emailTo;
+    private String emailSubject;
+    private String body;
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getEmailTo() {
+        return emailTo;
+    }
+
+    public void setEmailTo(String emailTo) {
+        this.emailTo = emailTo;
+    }
+
+    public String getEmailSubject() {
+        return emailSubject;
+    }
+
+    public void setEmailSubject(String emailSubject) {
+        this.emailSubject = emailSubject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
