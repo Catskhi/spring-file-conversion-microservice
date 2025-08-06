172dceb9b52124a0406c0dc7baa33770536edcad
catskhi <catskhi@proton.me>
2025-08-06 20:33:29 -0300

    refactor: changed queueName to exchangeName to reduce unnecessary code

--- user/src/main/java/com/catskhi/user/configuration/RabbitMqConfig.java
+++ user/src/main/java/com/catskhi/user/configuration/RabbitMqConfig.java
@@ -1,18 +1,18 @@
 package com.catskhi.user.configuration;
 
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
-    private final String queueName = "email-queue";
+    private final String exchangeName = "email";
     private final String videoProcessingQueue = "video-processing-queue";
 
     @Bean
-    public Queue userQueue() {
-        return new Queue(queueName, true);
+    public DirectExchange emailExchange() {
+        return new DirectExchange(exchangeName, true, false);
     }
 
     @Bean
