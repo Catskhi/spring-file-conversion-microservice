98fa43a861b7b17bf3369336662a5e4767288b0c
catskhi <catskhi@proton.me>
2025-08-06 20:29:36 -0300

    feat: added a method to consume the video-email-queue rabbitmq queue

--- mail/src/main/java/com/catskhi/mail/consumer/EmailConsumer.java
+++ mail/src/main/java/com/catskhi/mail/consumer/EmailConsumer.java
@@ -1,5 +1,6 @@
 package com.catskhi.mail.consumer;
 
+import com.catskhi.mail.configuration.RabbitMqConfiguration;
 import com.catskhi.mail.domain.EmailModel;
 import com.catskhi.mail.dto.EmailDto;
 import com.catskhi.mail.service.EmailService;
@@ -15,16 +16,29 @@ public class EmailConsumer {
     @Autowired
     final EmailService emailService;
 
-    public EmailConsumer(EmailService emailService) {
+    @Autowired
+    final RabbitMqConfiguration rabbitMqConfig;
+
+    public EmailConsumer(EmailService emailService, RabbitMqConfiguration rabbitMqConfig) {
         this.emailService = emailService;
+        this.rabbitMqConfig = rabbitMqConfig;
     }
 
-    @RabbitListener(queues = "email-queue")
-    public void consumeEmailQueue(@Payload EmailDto emailDto) {
-        System.out.println("Received message from email-queue: " + emailDto);
+    @RabbitListener(queues = "user-email-queue")
+    public void consumeUserCreatedEvent(@Payload EmailDto emailDto) {
+        System.out.println("Received 'created' event from user-email-queue: " + emailDto);
         var email = new EmailModel();
         BeanUtils.copyProperties(emailDto, email);
         emailService.sendAndSaveEmail(email);
         System.out.println("Sent email to: " + email.getEmailTo());
+    }
+
+    @RabbitListener(queues = "video-email-queue")
+    public void consumeVideoProcessingDoneEvent(@Payload EmailDto emailDto) {
+        System.out.println("Received 'video-processing-done' event from video-email-queue: " + emailDto);
+        var email = new EmailModel();
+        BeanUtils.copyProperties(emailDto, email);
+        emailService.sendAndSaveEmail(email);
+        System.out.println("Sent video processing done email to: " + email.getEmailTo());
     }
 }
